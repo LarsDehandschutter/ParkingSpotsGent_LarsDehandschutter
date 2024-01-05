6107d7aa7e1c0dacd62a532a0bb64dcbace0fb15
LarsDehandschutter <61112786+LarsDehandschutter@users.noreply.github.com>
2024-01-05 20:04:27 +0100

    Delete wrong ParkingspotsGent directory

--- ParkingspotsGent/app/src/main/java/examen/parkingspotsgent/data/RealTimeParkingSpotInfo.kt
+++
@@ -1,9 +0,0 @@
-package examen.parkingspotsgent.data
-
-// Real time parking info that is useful for the application
-data class RealTimeParkingSpotInfo(
-    val name: String,
-    val availableSpaces: Int,
-    val lat: String,
-    val lon: String
-)
--- ParkingspotsGent/app/src/main/java/examen/parkingspotsgent/model/Properties.kt
+++
@@ -1,7 +0,0 @@
-package examen.parkingspotsgent.model
-
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-class Properties
